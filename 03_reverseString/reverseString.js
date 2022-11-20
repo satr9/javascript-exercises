4c2be5c69ecdf74cc0b9a33abe41733d83d61bcb
satr9 <110054210+satr9@users.noreply.github.com>
2022-11-20 13:51:28 +0400

    Solve exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    let array = [];
+    for (let i = 0; i < string.length; i++) {
+        array.unshift(string.charAt(i));
+    }
+    return array.join("");
 };
 
 // Do not edit below this line
