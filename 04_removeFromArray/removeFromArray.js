10e451a413e9041b9fc6e8c919def82d97ffec65
satr9 <110054210+satr9@users.noreply.github.com>
2022-11-20 14:56:20 +0400

    Solve only first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, badItem) {
+    const newArray = array.filter(removeBadItem);
+    function removeBadItem(item) {
+        return item !== badItem;
+    }
+    return newArray;
 };
 
 // Do not edit below this line
