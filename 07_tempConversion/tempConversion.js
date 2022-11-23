589270d8751c59df0d025e69f31b04bda2048930
satr9 <110054210+satr9@users.noreply.github.com>
2022-11-23 20:52:08 +0400

    Solve blah blah

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (degreeF) {
+    let tempC = (degreeF - 32) * (5 / 9);
+    return Math.round(tempC * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (degreeC) {
+    let tempF = degreeC * (9 / 5) + 32;
+    return Math.round(tempF * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof,
 };
